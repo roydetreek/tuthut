4d17f25184870ac6211d25fb95ef82992abe639d
timodewinter <timodewinter@gmail.com>
2013-11-18 12:06:20 +0100

    edited a comment

--- save.php
+++ save.php
@@ -3,7 +3,8 @@
 // MIT License   - https://www.webrtc-experiment.com/licence/
 // Documentation - https://github.com/muaz-khan/WebRTC-Experiment/tree/master/RecordRTC
 
-//new comment
+//new comment, and a new line
+
 foreach(array('video', 'audio') as $type) {
     if (isset($_FILES["${type}-blob"])) {
         
