7adbe53e2bf320f734b042d1f6f99f3937ed756d
Timo de Winter <timodewinter@gmail.com>
2013-12-04 12:17:05 +0100

    updated db structure

--- dbstructure.sql
+++ dbstructure.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 28, 2013 at 12:09 PM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.12
+-- Generation Time: Dec 04, 2013 at 12:16 PM
+-- Server version: 5.5.31
+-- PHP Version: 5.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `nom`
 --
+CREATE DATABASE IF NOT EXISTS `nom` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `nom`;
 
 -- --------------------------------------------------------
 
@@ -40,8 +42,11 @@ CREATE TABLE IF NOT EXISTS `videos` (
   `Happiness` int(11) NOT NULL,
   `Amusing` int(11) NOT NULL,
   `Rating` tinyint(4) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Description` varchar(2000) NOT NULL,
+  `Featured` tinyint(1) NOT NULL,
   UNIQUE KEY `ID` (`ID`,`Videosource`,`Thumbnailsource`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=51 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=80 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
